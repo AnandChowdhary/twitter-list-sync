cf97ff9f719c8f653e9337f5706fd4643d42ed9a
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-16 17:55:14 +0530

    :sparkles: Add following users to list

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,33 @@
-import { getInput, debug, setFailed, setOutput } from "@actions/core";
-import { getOctokit } from "@actions/github";
+import Twitter from "twitter-lite";
+import { setFailed } from "@actions/core";
 
-const token =
-  getInput("token") || process.env.GH_PAT || process.env.GITHUB_TOKEN;
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY as string,
+  consumer_secret: process.env.TWITTER_API_SECRET_KEY as string,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN as string,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string,
+});
 
-export const run = async () => {
-  if (!token) throw new Error("GitHub token not found");
-  const octokit = getOctokit(token);
-
-  const ms: string = getInput("milliseconds");
-  debug(`Waiting ${ms} milliseconds ...`);
-
-  debug(new Date().toTimeString());
-  await wait(parseInt(ms, 10));
-  debug(new Date().toTimeString());
-
-  setOutput("time", new Date().toTimeString());
+export const following = async () => {
+  const following: { users: Array<{ id_str: string; screen_name: string }> } = await client.get(
+    "/followers/list"
+  );
+  for await (const user of following.users) {
+    console.log(`Adding @${user.screen_name}`);
+    await client.post("/lists/members/create", {
+      list_id: process.env.FOLLOWING_LIST,
+      user_id: user.id_str,
+    });
+    if (process.env.REMOVE_AFTER_ADDING) {
+      await client.post("/friendships/destroy", {
+        user_id: user.id_str,
+      });
+    }
+  }
 };
 
-export const wait = (milliseconds: number) => {
-  return new Promise((resolve) => setTimeout(() => resolve(), milliseconds));
+export const run = async () => {
+  await following();
 };
 
 run()
